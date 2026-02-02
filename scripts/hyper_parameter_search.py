7f75777732fdb9ca6a6a2258df922c200b4b6f67
Häntze <15168710+hhaentze@users.noreply.github.com>
2026-02-02 22:31:09 +0100

    Add MLP as model option

--- scripts/hyper_parameter_search.py
+++ scripts/hyper_parameter_search.py
@@ -13,22 +13,27 @@ from tqdm.auto import tqdm
 
 from renal_vision.modeling.eval import run_evaluation
 from renal_vision.modeling.train import run_training
-from renal_vision.shared.utils import generate_patient_fold_mapping
+from renal_vision.shared.utils import describe_data, generate_patient_fold_mapping
 
 # --- 1. Configuration & Search Space ---
 N_FOLDS = 10  # Reporting performance
-PARAM_GRID = list(
-    ParameterGrid(
-        {
-            "max_depth": [3, 4, 6],
-            "learning_rate": [0.01, 0.05, 0.1],
-            "gamma": [0, 1, 5],
-            "min_child_weight": [4, 12, 8],
-            "subsample": [0.7, 0.9],
-            "colsample_bytree": [0.7, 0.9],
-        }
-    )
-)
+
+xgb_params = {
+    "max_depth": [4, 6],
+    "learning_rate": [0.01, 0.05, 0.1],
+    "gamma": [0, 1, 5],
+    "min_child_weight": [4, 8, 12],
+    "subsample": [0.7, 0.9],
+    "colsample_bytree": [0.7, 0.9],
+}
+mlp_params = {
+    "hidden_layer_sizes": [(100,), (256,), (256, 128)],  # (100,) is standard
+    "alpha": [0.0001, 0.001, 0.01],  # L2 regularization term
+    "learning_rate_init": [0.001, 0.01],
+    "batch_size": [64, 128],
+}
+
+configs = [("mlp", list(ParameterGrid(mlp_params))), ("xgboost", list(ParameterGrid(xgb_params)))]
 tempfile.tempdir = "/tmp"
 
 
@@ -39,9 +44,15 @@ def main(
     output_csv: str,
     group_col: str = "case",
     stratify_col: str = "class_id",
+    aug_number: int = -1,
 ) -> None:
     # load features
     features = pd.read_parquet(feature_path)
+
+    # decide on how many augmentations to include
+    if aug_number > -1:
+        features = features[features["aug_id"] <= aug_number].reset_index(drop=True)
+    describe_data(features, target_column=stratify_col)
 
     # assign partitions to features
     fold_map = generate_patient_fold_mapping(
@@ -52,44 +63,45 @@ def main(
     config_results: Dict[str, List[float]] = {}
     param_lookup: Dict[str, Dict[str, Any]] = {}
 
-    n_loops = N_FOLDS * len(PARAM_GRID)
+    n_loops = sum([N_FOLDS * len(c[1]) for c in configs])
     with tqdm(total=n_loops, desc="Evaluating hyperparameters") as pbar:
-        for f in range(N_FOLDS):
-            with tempfile.TemporaryDirectory() as tmpdirname:
-                # Split
-                it_path = join(tmpdirname, "tmp_it.parquet")
-                iv_path = join(tmpdirname, "tmp_iv.parquet")
-                features[features["fold"] != f].to_parquet(it_path, index=False)
-                features[(features["fold"] == f) & (~features["augmented"])].to_parquet(
-                    iv_path, index=False
-                )
-
-                for _, params in enumerate(PARAM_GRID):
-                    param_key = json.dumps(params, sort_keys=True)
-                    if param_key not in param_lookup:
-                        param_lookup[param_key] = params
-                        config_results[param_key] = []
-
-                    # Train and Eval
-                    run_training(
-                        it_path,
-                        tmpdirname,
-                        model_type="xgboost",
-                        class_config_path=class_config_path,  # "names_binary.json",
-                        extractor_config_path=extractor_config_path,  # "/sc-scratch/sc-scratch-cc06-ag-ki-radiologie/kidney/embeddings/new/kits_radiomics.config.json",
-                        verbose=False,
-                        class_column=stratify_col,
-                        **params,
+        for model_name, param_grid in configs:
+            for f in range(N_FOLDS):
+                with tempfile.TemporaryDirectory() as tmpdirname:
+                    # Split
+                    it_path = join(tmpdirname, "tmp_it.parquet")
+                    iv_path = join(tmpdirname, "tmp_iv.parquet")
+                    features[features["fold"] != f].to_parquet(it_path, index=False)
+                    features[(features["fold"] == f) & (~features["augmented"])].to_parquet(
+                        iv_path, index=False
                     )
-                    m = run_evaluation(
-                        iv_path,
-                        join(tmpdirname, "model.pkl"),
-                        tmpdirname,
-                        class_column=stratify_col,
-                        verbose=False,
-                    )
-                    config_results[param_key].append(m["f1"])
-                    pbar.update(1)
+
+                    for _, params in enumerate(param_grid):
+                        param_key = json.dumps(params | {"model": model_name}, sort_keys=True)
+                        if param_key not in param_lookup:
+                            param_lookup[param_key] = params
+                            config_results[param_key] = []
+
+                        # Train and Eval
+                        run_training(
+                            it_path,
+                            tmpdirname,
+                            model_type=model_name,
+                            class_config_path=class_config_path,  # "names_binary.json",
+                            extractor_config_path=extractor_config_path,  # "/sc-scratch/sc-scratch-cc06-ag-ki-radiologie/kidney/embeddings/new/kits_radiomics.config.json",
+                            verbose=False,
+                            class_column=stratify_col,
+                            **params,
+                        )
+                        m = run_evaluation(
+                            iv_path,
+                            join(tmpdirname, "model.pkl"),
+                            tmpdirname,
+                            class_column=stratify_col,
+                            verbose=False,
+                        )
+                        config_results[param_key].append(m["f1"])
+                        pbar.update(1)
 
         # --- 3. Analysis and Ranking ---
         ranking_data = []
@@ -131,6 +143,12 @@ if __name__ == "__main__":
     parser.add_argument(
         "--stratify_col", default="class_id", help="Stratify column name (default: class_id)"
     )
+    parser.add_argument(
+        "--aug_number",
+        type=int,
+        default=-1,
+        help="How many augmentations should be included? (default: all)",
+    )
 
     args = parser.parse_args()
 
@@ -141,4 +159,5 @@ if __name__ == "__main__":
         output_csv=args.output_csv,
         group_col=args.group_col,
         stratify_col=args.stratify_col,
+        aug_number=args.aug_number,
     )
