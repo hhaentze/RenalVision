1cfe7adbd224540a42612c5af44b04d86c0924dc
Häntze <15168710+hhaentze@users.noreply.github.com>
2026-02-24 18:28:24 +0100

    Update radiomics  number in integration test

--- tests/renal_vision/features/test_intergration.py
+++ tests/renal_vision/features/test_intergration.py
@@ -5,7 +5,7 @@ import pytest
 # Centralized configuration: (name, import_path, class_name, expected_len)
 EXTRACTOR_DATA = [
     pytest.param(
-        "radiomics", "renal_vision.features.embeddings_radiomics", "RadiomicsExtractor", 88
+        "radiomics", "renal_vision.features.embeddings_radiomics", "RadiomicsExtractor", 107
     ),
     pytest.param("mevis", "renal_vision.features.embeddings_mevis", "MevisExtractor", 1024),
     pytest.param("fmcib", "renal_vision.features.embeddings_fmcib", "FMCIBExtractor", 4096),
@@ -21,39 +21,6 @@ def get_extractor(module_path, class_name, **kwargs):
 
 
 class TestFeatureExtraction:
-    # @pytest.mark.local_only
-    # @pytest.mark.integration
-    # @pytest.mark.parametrize("is_augmented", [True, False])
-    # @pytest.mark.parametrize(
-    #     "name, mod_path, cls_name, expected_emb_len",
-    #     EXTRACTOR_DATA,
-    #     ids=[item[0][0] for item in EXTRACTOR_DATA],
-    # )
-    # def test_end_to_end_extraction(
-    #     self, name, mod_path, cls_name, expected_emb_len, is_augmented, mock_single_lesion
-    # ):
-    #     # 1. Setup
-    #     image, seg = mock_single_lesion
-    #     extractor = get_extractor(mod_path, cls_name, min_volume=10)
-
-    #     # 2. Execution
-    #     embeddings = extractor.extract(image, seg, augment=is_augmented)
-
-    #     # 3. Validation
-    #     assert len(embeddings) == 1, f"{name} should return exactly one embedding for one lesion"
-
-    #     result = embeddings[0]
-    #     assert result["augmented"] == is_augmented
-
-    #     # The +5 represents metadata fields (ID, label, etc.)
-    #     expected_total_len = expected_emb_len + 5
-    #     actual_total_len = len(result)
-
-    #     assert actual_total_len == expected_total_len, (
-    #         f"Feature length mismatch for {name}. "
-    #         f"Expected {expected_total_len} (inc. metadata), got {actual_total_len}"
-    #     )
-
     @pytest.mark.local_only
     @pytest.mark.integration
     @pytest.mark.parametrize(
